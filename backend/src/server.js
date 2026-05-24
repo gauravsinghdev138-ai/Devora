dd2065e7a0f8f00c37adfbfab89f332d9670a380
GAurAv64651 <149515000+GAurAv64651@users.noreply.github.com>
2026-05-24 22:54:05 +0530

    deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,32 @@
 import express from "express";
+import path from "path";
 import {ENV} from "./lib/env.js";
 
 
 const app=express()
 
-app.get("/", (req,res)=>{
-    res.send("Running");
+
+const __dirname=path.resolve();
+
+app.get("/status", (req,res)=>{
+    res.send("Running like a cheetah...");
 })
 
+
+
+
+//Deployement to express serve the frontend
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
+  });
+}
+
 app.listen(ENV.PORT,()=>{
-    console.log("Server IS Running");
+    console.log("Server IS Running",ENV.PORT);
 })  
