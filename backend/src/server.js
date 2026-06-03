fab3c73447ed7b7601f5ce9f5231095ee1b35754
GAurAv64651 <149515000+GAurAv64651@users.noreply.github.com>
2026-06-03 06:30:44 +0530

    server-side-auth-and-token-genera

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import path from "path";
 import cors from 'cors';
 import {serve} from "inngest/express";
+import{ clerkMiddleware } from '@clerk/express'
 import {ENV} from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+import chatRoutes from "../routes/chatRoutes.js"
 
 
 const app=express()
@@ -14,14 +17,24 @@ const __dirname=path.resolve();
  
 //middleware
 app.use(express.json())
+
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 //allows browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
 app.use("/api/inngest",serve({ client: inngest, functions}) );
 
-app.get("/status", (req,res)=>{
-    res.send("Running like a cheetah...");
-})
+app.use("api/chat",chatRoutes);
+
+app.get("/video-call",protectRoute ,(req,res)=>{
+      
+  res.status(200).json({message:"video-call endpoint"});
+    
+ });
+
+ 
+
+
 
 
 
